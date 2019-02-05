3c97f80c020f9bc39774607a50a54aecd2cd3d79
Matt Areddia <mareddia@protonmail.com>
2019-02-05 13:55:16 -0600

    refactoring toastify-msg-renderer.tsx utility function

--- GhostUI/ClientApp/utils/toastify-msg-renderer.tsx
+++ GhostUI/ClientApp/utils/toastify-msg-renderer.tsx
@@ -6,11 +6,11 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 /**
  * Function that returns a JSX.Element - referenced in toastify methods to format messages with icon
  */
-export const renderToastContent = (msg: string, icon: string = toastContent.DEFAULT_ICON): React.ReactNode => (
+export const renderToastContent = (message: string, icon: string = toastContent.DEFAULT_ICON): React.ReactNode => (
     <div>
         <span className='icon' style={toastContent.ICON_STYLE}>
             <FontAwesomeIcon icon={icon as IconProp} />
         </span>
-        <span style={toastContent.MSG_STYLE}>{msg || ''}</span>
+        <span style={toastContent.MSG_STYLE}>{message}</span>
     </div>
 );
