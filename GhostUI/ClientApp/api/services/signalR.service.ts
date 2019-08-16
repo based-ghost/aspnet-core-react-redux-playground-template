8c185f33c85603aca0b8b77038ec8ad3eeb4afe9
Matt Areddia <mareddia@protonmail.com>
2019-08-16 15:05:48 -0500

    Updating signalR.service.ts service class to use features from newer package version

--- GhostUI/ClientApp/api/services/signalR.service.ts
+++ GhostUI/ClientApp/api/services/signalR.service.ts
@@ -1,14 +1,13 @@
 ﻿import { toast } from 'react-toastify';
-import * as SignalR from '@aspnet/signalr';
 import { signalRConfig } from '../../config/constants';
 import { renderToastifyMsg } from '../../utils/notificationUtils';
+import { HubConnection, HubConnectionBuilder, HubConnectionState } from '@aspnet/signalr';
 
 /**
  * SignalR API abstraction layer communication - configures/manages hub connections (typescript singleton pattern)
  */
 class SignalRService {
-    private _isConnected: boolean = false;
-    private _hubConnection!: SignalR.HubConnection;
+    private _hubConnection!: HubConnection;
     private static _signalRService: SignalRService;
 
     private constructor() {
@@ -21,29 +20,18 @@ class SignalRService {
     }
 
     public startConnection(): void {
-        // HubConnection.state does not exist in versions of  <= 1.0.0
-        // Had to downgrade since there is an existing bug around server-side rendering and some typeings
-        // Also need to define absolute path for hub url and not just /hubs/users in lower packages
-        // Uncomment after bug is resolved and package @aspnet/signalr is upgraded to latest / remove _isConnected code
-        //if (this._hubConnection.state === SignalR.HubConnectionState.Connected)
-        //    return;
-
-        if (this._isConnected)
+        if (this._hubConnection.state === HubConnectionState.Connected)
             return;
 
         setTimeout(() => {
-            this._hubConnection.start()
-                .then(() => {
-                    this._isConnected = true;
-                })
-                .catch((error) => {
-                    console.error(error.toString());
-                });
+            this._hubConnection.start().catch((error) => {
+                console.error(error.toString());
+            });
         }, signalRConfig.CONNECTION_DELAY);
     }
 
     private createConnection(): void {
-        this._hubConnection = new SignalR.HubConnectionBuilder()
+        this._hubConnection = new HubConnectionBuilder()
             .withUrl(signalRConfig.BASE_URL)
             .build();
     }
@@ -64,7 +52,6 @@ class SignalRService {
         this._hubConnection.on(signalRConfig.CLOSE_EVENT, (reason: string) => {
             this._hubConnection.stop().then(() => {
                 setTimeout(() => {
-                    this._isConnected = false;
                     toast.info(renderToastifyMsg(`All hub connections closed (SignalR) - ${reason}`, signalRConfig.TOASTIFY_ICON));
                 }, signalRConfig.HUB_MESSAGE_DELAY);
             });
