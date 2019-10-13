16bd220f298072214bdc2cbee3a4b5d4d32bb530
Matt Areddia <mareddia@protonmail.com>
2019-10-13 08:43:13 -0500

    feature: adding styled-components and a usage example with the Spinner.tsx component; updated README with tag for styled-components

--- GhostUI/ClientApp/components/loaders/Spinner.tsx
+++ GhostUI/ClientApp/components/loaders/Spinner.tsx
@@ -1,19 +1,64 @@
 ﻿import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 type SpinnerProps = {
   readonly loading?: boolean;
 };
 
+type StyledSpinnerProps = {
+  readonly $loading?: boolean;
+};
+
+const rotateAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+  } 100% {
+    transform: rotate(360deg);
+  }
+`;
+
+const borderColor = '#209cee transparent transparent transparent';
+
+const StyledSpinner = styled.div<StyledSpinnerProps>`
+  top: 50%;
+  left: 48%;
+  z-index: 9999;
+  width: 4.75em;
+  height: 4.75em;
+  position: absolute;
+  display: ${({ $loading }) => $loading ? 'inline-block' : 'none'};
+
+  > div {
+    width: 4.75em;
+    height: 4.75em;
+    position: absolute;
+    border-radius: 50%;
+    border: 0.35em solid;
+    box-sizing: border-box;
+    border-color: ${borderColor};
+    animation: ${rotateAnimation} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+
+    :nth-child(1) {
+       animation-delay: -0.45s;
+    }
+
+    :nth-child(2) {
+      animation-delay: -0.3s;
+    }
+
+    :nth-child(3) {
+      animation-delay: -0.15s;
+    }
+  }
+`;
+
 const Spinner = React.memo<SpinnerProps>(({ loading }) => (
-  <div
-    id='load-spinner'
-    style={{ display: loading ? 'inline-block' : 'none' }}
-  >
+  <StyledSpinner $loading={loading}>
     <div />
     <div />
     <div />
     <div />
-  </div>
+  </StyledSpinner>
 ));
 
 export default Spinner;
--- README.md
+++ README.md
@@ -25,6 +25,7 @@ After discovering the ```dotnet new reactredux``` command no longer created the 
   - [TypeScript](https://www.typescriptlang.org/)
   - [Webpack](https://github.com/webpack/webpack) for bundling of application assets and HMR (Hot Module Replacement)
   - [Bulma CSS Framework](https://bulma.io/) + [SASS](https://github.com/sass/sass) + Font Awesome 5 (using fontawesome-svg-core)
+  - [styled-components](https://www.styled-components.com/) - CSS in JavaScript via template literals - example usage is demonstrated in the Spinner.tsx component.
   - [autobind-decorator](https://github.com/andreypopp/autobind-decorator) which is self-described as 'A class or method decorator which binds methods to the instance so this is always correct, even when the method is detached' - nice-to-have syntax that makes your code more succintc and avoids the repetition of having to bind methods in the constructor of a class-component - autobind (boundMethod) on a method is lazy and is only bound once.
   - [Axios](https://github.com/axios/axios) for REST endpoint requests
   - [react-toastify](https://github.com/fkhadra/react-toastify) - a highly configurable toast notification library - comes hooked up to display login error & SignalR hub push notifications examples.
