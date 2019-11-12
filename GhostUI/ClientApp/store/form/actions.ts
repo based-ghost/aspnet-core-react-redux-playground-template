3892ae2c9f4a504f2c8d0d62d97966c3c8f63fe2
Matt Areddia <mareddia@protonmail.com>
2019-11-11 18:11:33 -0600

    bump node dev dependencies and dependencies

--- GhostUI/ClientApp/store/form/actions.ts
+++ GhostUI/ClientApp/store/form/actions.ts
@@ -2,18 +2,18 @@
 import { ActionType, IDropdownOption } from './types';
 
 export const actionCreators = {
-    increment: (): ReduxAction => ({
-      type: ActionType.INCREMENT
-    }),
-    decrement: (): ReduxAction => ({
-      type: ActionType.DECREMENT
-    }),
-    handleOnCheck: (checked: boolean): ReduxAction => ({
-      checked: checked,
-      type: ActionType.CHECK_SAMPLE_BOX
-    }),
-    selectOption: (option: IDropdownOption): ReduxAction => ({
-      option: option,
-      type: ActionType.DROPDOWN_SELECT
-    })
+  increment: (): ReduxAction => ({
+    type: ActionType.INCREMENT
+  }),
+  decrement: (): ReduxAction => ({
+    type: ActionType.DECREMENT
+  }),
+  handleOnCheck: (checked: boolean): ReduxAction => ({
+    checked: checked,
+    type: ActionType.CHECK_SAMPLE_BOX
+  }),
+  selectOption: (option: IDropdownOption): ReduxAction => ({
+    option: option,
+    type: ActionType.DROPDOWN_SELECT
+  })
 };
