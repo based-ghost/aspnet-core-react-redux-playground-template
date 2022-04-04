fab234054da99bedcc5099b03f0e9e8c0cca57af
Matt Areddia <mareddia@protonmail.com>
2022-04-03 23:50:35 -0500

    bump node deps/dev deps; bump .NET packages; bump node deps/dev deps; upgrade index.tsx file to utilize new root API made available since upgrading to React 18; move SignalR hub creation (client) to index.tsx from App.tsx

--- GhostUI/ClientApp/src/App.tsx
+++ GhostUI/ClientApp/src/App.tsx
@@ -1,21 +1,13 @@
-import { useEffect } from 'react';
 import Layout from './Layout';
 import { hot } from 'react-hot-loader/root';
 import { Route, Routes } from 'react-router';
 import { useLocation } from 'react-router-dom';
-import { SignalRApi } from './api/signalr.service';
 import { Routes as routes, TRANSITION_DEFAULT } from './config';
 import { CSSTransition, SwitchTransition } from 'react-transition-group';
 
 import type { FunctionComponent } from 'react';
 
 const App: FunctionComponent = () => {
-  useEffect(() => {
-    setTimeout(async () => {
-      await SignalRApi.startConnection();
-    }, 250);
-  }, []);
-
   const location = useLocation();
   const cssKey = location.pathname?.split('/')[1] || '/';
   const curRoute = routes.find((x) => (x.path === cssKey) || (x.name.toLowerCase() === cssKey.toLowerCase()));
--- GhostUI/ClientApp/src/index.tsx
+++ GhostUI/ClientApp/src/index.tsx
@@ -1,12 +1,13 @@
 import 'react-hot-loader'; // Must be imported befire React and ReactDOM
-import { render } from 'react-dom';
-import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { StrictMode, useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './assets/style/scss/site.scss';
 import './config/fa.config';
 import { toastifyProps } from './config';
+import { SignalRApi } from './api/signalr.service';
 import { configureStore, RootState } from './store';
 import { ToastContainer } from 'react-toastify';
 import reportWebVitals from './reportWebVitals';
@@ -15,17 +16,29 @@ import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 const initialState: RootState = (window as any)?.initialReduxState;
 const store = configureStore(initialState);
 
-render(
-  <StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </BrowserRouter>
-    <ToastContainer {...toastifyProps} />
-  </StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root') as Element | DocumentFragment;
+const root = createRoot(container);
+
+function AppRenderer(): JSX.Element {
+  useEffect(() => {
+    setTimeout(async () => {
+      await SignalRApi.startConnection();
+    }, 250);
+  }, []);
+
+  return (
+    <StrictMode>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+      <ToastContainer {...toastifyProps} />
+    </StrictMode>
+  );
+}
+
+root.render(<AppRenderer />);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
